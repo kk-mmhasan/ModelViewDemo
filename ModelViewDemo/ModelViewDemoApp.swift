2ee293d382d1c9ffb8e5abfba685f50ac18f7e57
Mahmudul Hasan <mahmudul.hasan@bitflyer.com>
2023-08-07 14:34:30 +0900

    Initial Commit

---
+++ ModelViewDemo/ModelViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewDemoApp.swift
+//  ModelViewDemo
+//
+//  Created by mahmudul.hasan on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct ModelViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
